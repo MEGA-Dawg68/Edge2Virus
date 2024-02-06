8783bd60cfa4a898ca2dcadbf698bd57ff98c500
97-42 <144976307+97-42@users.noreply.github.com>
2024-02-06 09:22:28 +0800

    Update Background.py
    nuh hu

--- Edge2Virus/Background.py
+++ Edge2Virus/Background.py
@@ -1,45 +1,28 @@
 import pygame
-import itertools
+import random
 
-def create_blue_screen():
-    # Initialize the pygame
+def create_infinite_colors_screen():
+    # Initialize pygame
     pygame.init()
 
     # Set resolution higher than the computer resolution so it is full screen and windows can overlap
     screen = pygame.display.set_mode((3840, 2160), pygame.RESIZABLE)
-    
 
-    # Feel free to add more colors
-    colors = [(255, 0, 0), (0, 255, 0), (0, 0, 255), (255, 255, 0), (0, 255, 255), (255, 0, 255), (255, 255, 255), (0, 0, 0)]
-    color_iterator = itertools.cycle(colors)
+    clock = pygame.time.Clock()
 
-    # current color
-    current_color = next(color_iterator)
-
-    
-    screen.fill(current_color)
-
-    # Update the display
-    pygame.display.flip()
-
-    
-    start_ticks = pygame.time.get_ticks()
-
-    
     running = True
     while running:
-        seconds = (pygame.time.get_ticks() - start_ticks) / 1000 
-        if seconds > 0.01: # 0.1 seconds
-            current_color = next(color_iterator)
-            screen.fill(current_color)
-            pygame.display.flip()
-            start_ticks = pygame.time.get_ticks()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
 
-    
+        # Generate random RGB values for the color
+        current_color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+        screen.fill(current_color)
+        pygame.display.flip()
+
+        clock.tick(50000)  # Adjust the frame rate (frames per second) as needed
+
     pygame.quit()
 
-
-create_blue_screen()
+create_infinite_colors_screen()
